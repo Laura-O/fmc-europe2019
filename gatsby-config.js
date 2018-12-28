649a03f850dc38662061cc0f45c25d21ab803a85
Laura Ohrndorf <laura.ohrndorf@googlemail.com>
2018-12-28 15:05:49 +0100

    Schedule (#13)

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,11 @@ module.exports = {
         icon: 'src/images/cube.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     //'gatsby-plugin-offline',

2541d9259f023e7b98fc4fe8c31e21878ca20c1d
Laura Ohrndorf <laura.ohrndorf@grandcentrix.net>
2018-12-28 02:27:04 +0100

    Change logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,16 +25,16 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'gatsby-starter-default',
-        short_name: 'starter',
+        short_name: 'fmc-europe2018',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/cube.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    //'gatsby-plugin-offline',
   ],
 }
